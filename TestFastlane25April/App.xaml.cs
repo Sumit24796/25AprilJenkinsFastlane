ed25f7f7af93c134482a6db5f3c539ebcac861fe
“Sumit Singh” <susingh@dminc.com>
2022-05-05 11:32:28 +0530

    test

--- TestFastlane25April/App.xaml.cs
+++ TestFastlane25April/App.xaml.cs
@@ -8,7 +8,6 @@ namespace TestFastlane25April
     {
         public App()
         {
-            //rajugentleman
             InitializeComponent();
             MainPage = new MainPage();
         }
